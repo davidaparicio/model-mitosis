ecd572a1b7d5df123b5d118b167532aa6b5ab75e
Julien Topçu <julien.topcu@gmail.com>
2019-06-11 11:50:23 +0000

    introducing Spring Boot in the infrastructure and fixing dependencies conflicts

---
+++ columbiad-express-infra/src/test/kotlin/org/craftsrecords/columbiadexpress/infractructure/ColumbiadExpressApplicationTests.kt
@@ -0,0 +1,17 @@
+package org.craftsrecords.columbiadexpress.infractructure
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT
+import org.springframework.test.context.junit.jupiter.SpringExtension
+
+
+@ExtendWith(SpringExtension::class)
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+class ColumbiadExpressApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
