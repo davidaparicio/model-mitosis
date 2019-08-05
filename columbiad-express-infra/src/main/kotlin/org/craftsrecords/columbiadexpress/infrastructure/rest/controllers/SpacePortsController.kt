8a1d4d5e36f27a60d7744f2ef9126cebbaeca878
Julien Topçu <julien.topcu@gmail.com>
2019-08-05 20:17:49 +0200

    Listing all available spaceports

--- columbiad-express-infra/src/main/kotlin/org/craftsrecords/columbiadexpress/infrastructure/rest/controllers/SpacePortsController.kt
+++ columbiad-express-infra/src/main/kotlin/org/craftsrecords/columbiadexpress/infrastructure/rest/controllers/SpacePortsController.kt
@@ -4,15 +4,20 @@ import org.craftsrecords.columbiadexpress.domain.spaceport.api.RetrieveSpacePort
 import org.craftsrecords.columbiadexpress.infrastructure.rest.resources.SpacePorts
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/spaceports")
 class SpacePortsController(private val retrieveSpacePorts: RetrieveSpacePorts) {
 
+
     @GetMapping
-    fun `retrieve all spaceports`(): SpacePorts {
-        val spacePorts = retrieveSpacePorts `having in their name` ""
+    fun `retrieve all spaceports having in their name`(
+            @RequestParam(name = "withNameContaining", required = false, defaultValue = "")
+            partialName: String
+    ): SpacePorts {
+        val spacePorts = retrieveSpacePorts `having in their name` partialName
         return SpacePorts(spacePorts)
     }
 }
--- columbiad-express-infra/src/test/kotlin/org/craftsrecords/columbiadexpress/infrastructure/rest/controllers/SpacePortsControllerShould.kt
+++ columbiad-express-infra/src/test/kotlin/org/craftsrecords/columbiadexpress/infrastructure/rest/controllers/SpacePortsControllerShould.kt
@@ -1,8 +1,7 @@
 package org.craftsrecords.columbiadexpress.infrastructure.rest.controllers
 
 import org.craftsrecords.columbiadexpress.infrastructure.configurations.DomainConfiguration
-import org.hamcrest.Matchers.equalTo
-import org.hamcrest.collection.IsCollectionWithSize
+import org.hamcrest.Matchers.containsInAnyOrder
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
@@ -12,7 +11,9 @@ import org.springframework.http.MediaType.APPLICATION_JSON_UTF8
 import org.springframework.test.context.junit.jupiter.SpringExtension
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
 @ExtendWith(SpringExtension::class)
 @WebMvcTest(controllers = [SpacePortsController::class])
@@ -23,13 +24,28 @@ class SpacePortsControllerShould {
     private lateinit var mvc: MockMvc
 
     @Test
-    fun `list all available Space Ports`() {
+    fun `list all available SpacePorts`() {
         mvc.perform(
                 get("/spaceports")
                         .accept(APPLICATION_JSON_UTF8))
                 //
                 .andExpect(status().isOk)
                 .andExpect(content().contentType(APPLICATION_JSON_UTF8))
-                .andExpect(jsonPath("$.spacePorts", IsCollectionWithSize<Any>(equalTo(3))))
+                .andExpect(jsonPath("$.spacePorts.length()").value(3))
+                .andExpect(jsonPath("$.spacePorts[0].id").isNotEmpty)
+                .andExpect(jsonPath("$.spacePorts[0].name").value("Vostochny Cosmodrome"))
+                .andExpect(jsonPath("$.spacePorts[0].location").value("EARTH"))
+    }
+
+    @Test
+    fun `list all SpacePorts having a name containing a given string`() {
+        mvc.perform(
+                get("/spaceports")
+                        .param("withNameContaining", "Cosmo")
+                        .accept(APPLICATION_JSON_UTF8))
+                //
+                .andExpect(status().isOk)
+                .andExpect(content().contentType(APPLICATION_JSON_UTF8))
+                .andExpect(jsonPath("$.spacePorts[*].name").value(containsInAnyOrder("Vostochny Cosmodrome", "Baikonur Cosmodrome")))
     }
 }
