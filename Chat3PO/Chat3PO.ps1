ca8442c0b0e99dc31f4c01a4d294802759cfcae1
Julien Topçu <julien.topcu@gmail.com>
2023-05-05 22:04:32 +0200

    Enhance Chat3PO

--- Chat3PO/Chat3PO.ps1
+++ Chat3PO/Chat3PO.ps1
@@ -1,6 +1,39 @@
-cd C:\Users\Julien\IdeaProjects\model-mitosis\backend
+Add-Type -AssemblyName presentationCore
+$player = New-Object System.Windows.Media.MediaPlayer
 
+Set-Location C:\Users\Julien\IdeaProjects\model-mitosis\backend
 Clear-Host
+function Typing($sentence) {
+
+    Write-Host "Chat3PO> " -NoNewLine -ForegroundColor Yellow
+    foreach ($char in $sentence.ToCharArray()) {
+        Write-Host -NoNewline $char
+        Start-Sleep -Milliseconds 50
+    }
+    Write-Host
+}
+
+function EverythingInSearchDomain() {
+
+    Write-Host "Chat3PO> " -NoNewLine -ForegroundColor Yellow
+    $part1="Most assuredly, the task was not without its challenges. The very software upon which we relied, Intellij, "
+    $part2="was most uncooperative, causing no small amount of frustration and delay"
+    foreach ($char in $part1.ToCharArray()) {
+        Write-Host -NoNewline $char
+        Start-Sleep -Milliseconds 50
+    }
+    Start-Sleep -Milliseconds 300
+    foreach ($char in $part2.ToCharArray()) {
+        Write-Host -NoNewline $char
+        Start-Sleep -Milliseconds 50
+    }
+    Write-Host
+}
+
+$sound=[uri]"C:\Users\Julien\IdeaProjects\model-mitosis\Chat3PO\hello-master.mp3"
+$player.Open($sound)
+$player.Play()
+Typing "Hello master, how may I be of service to you today?"
 
 do {
     Write-Host "Chat3PO> " -NoNewLine -ForegroundColor Yellow
@@ -9,9 +42,12 @@ do {
     if ($param.ToLower().Contains("everything")) {
         $branchName = Get-Date -Format "yyyy-MM-dd-HHmm"
         git checkout -b $branchName 2b26e8a4 2>&1 > $null
-        Write-Host "Done !"
-    }else{
+        $sound=[uri]"C:\Users\Julien\IdeaProjects\model-mitosis\Chat3PO\everything-in-search-domain.mp3"
+        $player.Open($sound)
+        $player.Play()
 
+        EverythingInSearchDomain
+    }else{
         Write-Host "nothing"
     }
 
