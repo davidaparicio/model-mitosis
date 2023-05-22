2fece87174062c47449f283505bf1ae3a436463d
Julien Topçu <julien.topcu@gmail.com>
2023-05-22 09:07:03 +0200

    Adding AntiCorruptionLayer annotation

---
+++ backend/mandalore-express-annotations/src/main/kotlin/com/beyondxscratch/mandaloreexpress/annotations/AntiCorruptionLayer.kt
@@ -0,0 +1,8 @@
+package com.beyondxscratch.mandaloreexpress.annotations
+
+import java.lang.annotation.Inherited
+
+@Retention(AnnotationRetention.RUNTIME)
+@Target(AnnotationTarget.CLASS)
+@Inherited
+annotation class AntiCorruptionLayer
